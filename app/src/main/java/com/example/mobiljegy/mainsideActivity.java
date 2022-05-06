0e3b91d4b2d0be38b20dd576accca50610e92758
hites <hitesbarnus@gmail.com>
2022-05-06 15:02:58 +0200

    Mobiljegy App

--- app/src/main/java/com/example/mobiljegy/mainsideActivity.java
+++ app/src/main/java/com/example/mobiljegy/mainsideActivity.java
@@ -1,12 +1,17 @@
 package com.example.mobiljegy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,6 +28,7 @@ public class mainsideActivity extends AppCompatActivity {
     private TicketItemAdapter mAdapter;
 
     private int gridNumber = 1;
+    private boolean viewRow = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,5 +69,68 @@ public class mainsideActivity extends AppCompatActivity {
         itemsImageResource.recycle();
 
         mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.ticket_list_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d(LOG_TAG, s);
+                mAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.view_selector:
+                Log.d(LOG_TAG, "View selector clicked");
+                if(viewRow) {
+                    changeSpanCount(item, R.drawable.ic_view, 1);
+                } else {
+                    changeSpanCount(item, R.drawable.ic_view_row, 2);
+                }
+                return true;
+            case R.id.setting_button:
+                Log.d(LOG_TAG, "Setting button clicked");
+                return true;
+            case R.id.logout_button:
+                Log.d(LOG_TAG, "Log out clicked");
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                return true;
+            case R.id.my_tickets:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void changeSpanCount(MenuItem item, int drawableId, int spanCount) {
+        viewRow = !viewRow;
+        item.setIcon(drawableId);
+        GridLayoutManager layoutManager = (GridLayoutManager) mRecyclerView.getLayoutManager();
+        layoutManager.setSpanCount(spanCount);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 }
